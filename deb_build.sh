55ec47b2647da98930b3e19cc29db58b80536747
Thomas Stringer <thstring@microsoft.com>
2019-10-16 08:49:27 -0400

    add build step to pipeline

--- deb_build.sh
+++ deb_build.sh
@@ -3,6 +3,6 @@
 VERSION=$(python3 -c "from sodigest.version import VERSION; print(VERSION, end='')")
 
 echo "$(date) - Building version $VERSION"
-dch --newversion "$VERSION"
+dch --newversion "$VERSION" ""
 
 debuild
