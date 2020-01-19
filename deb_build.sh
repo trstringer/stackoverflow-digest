793d204b22da729042dbd24503579b6b045eca95
Thomas Stringer <thstring@microsoft.com>
2020-01-19 15:07:24 -0500

    fix deb build script

--- deb_build.sh
+++ deb_build.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+set -e
+
 VERSION=$(python3 -c "from sodigest.version import VERSION; print(VERSION, end='')")
 
 echo "$(date) - Building version $VERSION"
-dch --release bionic --newversion "$VERSION" ""
+dch --newversion "$VERSION" "New release"
+dch --release bionic
 
 debuild -i -us -uc -S
